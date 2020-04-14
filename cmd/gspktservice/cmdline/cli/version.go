53a011b09ef5c47497b0039388794a656db7d3e4
rbr <rbr@arcor.de>
2020-04-14 21:21:27 +0200

    FEAT(all): overwork version data usage

--- cmd/gspktservice/cmdline/cli/version.go
+++ cmd/gspktservice/cmdline/cli/version.go
@@ -5,9 +5,18 @@ import (
 	"io"
 )
 
+// var section of global version variables
+var (
+	Version     = "0.1.0-snapshot"
+	Build       = "-"
+	serviceName = "gspktservice"
+	author      = "rbr <github.com/raibru>"
+	license     = "MIT License (c) 2020 raibru"
+)
+
 // PrintVersion prints the tool versions string
 func PrintVersion(w io.Writer) {
-	fmt.Fprintf(w, "gspktservice - v0.1.0\n")
-	fmt.Fprintf(w, "  author : rbr <raibru@web.de>\n")
-	fmt.Fprintf(w, "  license: MIT\n\n")
+	fmt.Fprintf(w, "%s - v%s (build:%s)\n", serviceName, Version, Build)
+	fmt.Fprintf(w, "  author : %s\n", author)
+	fmt.Fprintf(w, "  license: %s\n\n", license)
 }
