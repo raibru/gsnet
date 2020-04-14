51e3c86882aa3f04fd4d11acb2e0f7db8795b162
rbr <rbr@arcor.de>
2020-04-14 21:27:11 +0200

    FEAT(anycli): add quick-dirty client connection behavior

--- cmd/anyclient/cmdline/cli/root.go
+++ cmd/anyclient/cmdline/cli/root.go
@@ -1,7 +1,20 @@
 package cli
 
-var prtVersion bool
+import "github.com/raibru/gsnet/internal/service"
+
+var (
+	clientService service.ClientServiceData
+
+	prtVersion bool
+	inputfile  string
+	configfile string
+)
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&prtVersion, "version", "v", false, "display anxclient version")
+	rootCmd.PersistentFlags().StringVarP(&clientService.Addr, "address", "", "", "connect to Tcp/Ip address")
+	rootCmd.PersistentFlags().StringVarP(&clientService.Port, "port", "", "", "Port of address")
+	rootCmd.PersistentFlags().StringVarP(&clientService.Name, "name", "", "", "Name of the client service")
+	rootCmd.PersistentFlags().StringVarP(&inputfile, "input-file", "i", "", "Use input file send multible data packages")
+	rootCmd.PersistentFlags().StringVarP(&configfile, "config-file", "f", "", "Use config file for service behavior")
 }
