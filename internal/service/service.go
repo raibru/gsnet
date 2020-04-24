d891f567e35fbd7a7e96e888254f1de08c699894
rbr <rbr@arcor.de>
2020-04-24 16:58:22 +0200

    FEAT(service): overwork socket data exchange behavior

--- internal/service/service.go
+++ internal/service/service.go
@@ -72,14 +72,29 @@ func (s *ServerServiceData) ApplyConnection() error {
 	}
 
 	ctx.Log().Tracef("::: establish listener for service %s@%s:%s", s.Name, s.Addr, s.Port)
+
+	manager := ClientManager{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+
+	go manager.start()
+
 	for {
 		ctx.Log().Trace("::: wait for input...")
 		conn, err := lsn.Accept()
 		ctx.Log().Trace("::: accept input...")
 		if err != nil {
+			ctx.Log().Errorf("::: failure accept connection due '%s'", err.Error())
 			continue
 		}
-		go handleServerConnection(conn)
+		client := &Client{socket: conn, data: make(chan []byte)}
+		manager.register <- client
+		go manager.receive(client)
+		//go manager.send(client)
+		//go handleServerConnection(conn)
 		ctx.Log().Info("::: finish apply server listener")
 	}
 }
@@ -97,16 +112,19 @@ func (s *ClientServiceData) ApplyConnection() error {
 		return err
 	}
 
+	client := &Client{socket: conn}
+	go client.receive()
+	msg := "HALLO WORLD"
+
 	for i := range [10]int{} {
-		_, err = conn.Write([]byte("HALLO WORLD"))
+		_, err = conn.Write([]byte(msg))
 		if err != nil {
 			ctx.Log().Errorf("::: failure send data due '%s'", err.Error())
 			fmt.Fprintf(os.Stderr, "Error send data: %s\n", err.Error())
 			return err
 		}
-		ctx.Log().Tracef("::: successful send data %v time(s)", i)
+		ctx.Log().Tracef("::: successful send data %v time(s)", i+1)
 		time.Sleep(5 * time.Second)
-
 	}
 
 	ctx.Log().Info("::: finish apply client connection")
@@ -190,125 +208,99 @@ func handleServerConnection(conn net.Conn) {
 	}
 }
 
+// ClientManager hold communication behavior
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+// Client hold client communication behavior
+type Client struct {
+	socket net.Conn
+	data   chan []byte
+}
+
+func (manager *ClientManager) start() {
+	ctx.Log().Info("start manage client connections")
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			ctx.Log().Info("::: register client connection")
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				ctx.Log().Info("::: unregister terminated client connection")
+			}
+		case message := <-manager.broadcast:
+			ctx.Log().Info("::: broadcast to all managed client connections")
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					ctx.Log().Info("::: delete terminated client connections")
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+		ctx.Log().Info("::: finish manage client connections")
+	}
+}
+
+func (manager *ClientManager) receive(client *Client) {
+	ctx.Log().Info("receive data from managed client connections")
+	for {
+		msg := make([]byte, 4096)
+		length, err := client.socket.Read(msg)
+		if err != nil {
+			manager.unregister <- client
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			ctx.Log().Infof("received data [%s]", string(msg))
+			manager.broadcast <- msg
+		}
+	}
+	ctx.Log().Info("::: finish receive data")
+}
+
+func (client *Client) receive() {
+	ctx.Log().Info("receive data from client")
+	for {
+		msg := make([]byte, 4096)
+		length, err := client.socket.Read(msg)
+		if err != nil {
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			ctx.Log().Infof("received data [%s]", string(msg))
+		}
+	}
+	ctx.Log().Info("::: finish receive data")
+}
+
+func (manager *ClientManager) send(client *Client) {
+	defer client.socket.Close()
+	ctx.Log().Info("send data to managed client")
+	for {
+		select {
+		case msg, ok := <-client.data:
+			if !ok {
+				return
+			}
+			client.socket.Write(msg)
+		}
+	}
+	ctx.Log().Info("::: finish send data")
+}
+
 // // https://www.thepolyglotdeveloper.com/2017/05/network-sockets-with-the-go-programming-language/
-// type ClientManager struct {
-//     clients    map[*Client]bool
-//     broadcast  chan []byte
-//     register   chan *Client
-//     unregister chan *Client
-// }
-//
-// type Client struct {
-//     socket net.Conn
-//     data   chan []byte
-// }
-//
-// func startServerMode() { }
-//
-// func startClientMode() { }
-//
-// func main() {
-//     flagMode := flag.String("mode", "server", "start in client or server mode")
-//     flag.Parse()
-//     if strings.ToLower(*flagMode) == "server" {
-//         startServerMode()
-//     } else {
-//         startClientMode()
-//     }
-// }
-//
-// func (manager *ClientManager) start() {
-//     for {
-//         select {
-//         case connection := <-manager.register:
-//             manager.clients[connection] = true
-//             fmt.Println("Added new connection!")
-//         case connection := <-manager.unregister:
-//             if _, ok := manager.clients[connection]; ok {
-//                 close(connection.data)
-//                 delete(manager.clients, connection)
-//                 fmt.Println("A connection has terminated!")
-//             }
-//         case message := <-manager.broadcast:
-//             for connection := range manager.clients {
-//                 select {
-//                 case connection.data <- message:
-//                 default:
-//                     close(connection.data)
-//                     delete(manager.clients, connection)
-//                 }
-//             }
-//         }
-//     }
-// }
-//
-// func (manager *ClientManager) receive(client *Client) {
-//     for {
-//         message := make([]byte, 4096)
-//         length, err := client.socket.Read(message)
-//         if err != nil {
-//             manager.unregister <- client
-//             client.socket.Close()
-//             break
-//         }
-//         if length > 0 {
-//             fmt.Println("RECEIVED: " + string(message))
-//             manager.broadcast <- message
-//         }
-//     }
-// }
-//
-// func (manager *ClientManager) send(client *Client) {
-//     defer client.socket.Close()
-//     for {
-//         select {
-//         case message, ok := <-client.data:
-//             if !ok {
-//                 return
-//             }
-//             client.socket.Write(message)
-//         }
-//     }
-// }
-//
-// func startServerMode() {
-//     fmt.Println("Starting server...")
-//     listener, error := net.Listen("tcp", ":12345")
-//     if error != nil {
-//         fmt.Println(error)
-//     }
-//     manager := ClientManager{
-//         clients:    make(map[*Client]bool),
-//         broadcast:  make(chan []byte),
-//         register:   make(chan *Client),
-//         unregister: make(chan *Client),
-//     }
-//     go manager.start()
-//     for {
-//         connection, _ := listener.Accept()
-//         if error != nil {
-//             fmt.Println(error)
-//         }
-//         client := &Client{socket: connection, data: make(chan []byte)}
-//         manager.register <- client
-//         go manager.receive(client)
-//         go manager.send(client)
-//     }
-// }
-//
-// func (client *Client) receive() {
-//     for {
-//         message := make([]byte, 4096)
-//         length, err := client.socket.Read(message)
-//         if err != nil {
-//             client.socket.Close()
-//             break
-//         }
-//         if length > 0 {
-//             fmt.Println("RECEIVED: " + string(message))
-//         }
-//     }
-// }
 //
 // // We covered a lot of ground, so it might be easier to look at the application as a whole.
 // // Somewhere in your $GOPATH you’ll want a main.go file like previously mentioned. It should
