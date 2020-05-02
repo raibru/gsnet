78d14fca5e0774bc1260d83527b08c7c4e9f9817
rbr <rbr@arcor.de>
2020-05-02 22:26:15 +0200

    FEAT(arch): add achive functionality

--- internal/arch/arch.go
+++ internal/arch/arch.go
@@ -1,6 +1,15 @@
 package arch
 
-import "github.com/raibru/gsnet/internal/sys"
+import (
+	"encoding/csv"
+	"os"
+
+	"github.com/raibru/gsnet/internal/sys"
+)
+
+//
+// Context logging
+//
 
 type archLogger struct {
 	contextName string
@@ -24,4 +33,60 @@ func (l archLogger) ApplyLogger() error {
 
 func (archLogger) GetContextName() string {
 	return ctx.ContextName()
+}
+
+//
+// Archive handling
+//
+
+// ArchiveRecord holds send/receive data with meta info per record
+type ArchiveRecord struct {
+	MsgID        string
+	MsgDirection string // RX, TX
+	Protocol     string
+	Data         string
+}
+
+// Archive hold archive runable parameter
+type Archive struct {
+	Filename    string
+	ArchiveType string
+	DataChan    chan ArchiveRecord
+}
+
+// NewArchive create a new archive object to write archive records
+func NewArchive(name string, archType string) *Archive {
+	a := &Archive{
+		Filename:    name,
+		ArchiveType: archType,
+		DataChan:    make(chan ArchiveRecord),
+	}
+
+	return a
+}
+
+// Run run archiving in goroutine
+func (a *Archive) Run() {
+	go handleArchive(a)
+}
+
+// handleArchive archive data into configured archive destination
+func handleArchive(a *Archive) {
+	f, err := os.OpenFile(a.Filename, os.O_WRONLY|os.O_SYNC|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		ctx.Log().Errorf("Failure open/create archive file: %s", err.Error())
+		return
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	for rec := range a.DataChan {
+		data := []string{rec.MsgID, rec.MsgDirection, rec.Protocol, rec.Data}
+		if err := w.Write(data); err != nil {
+			ctx.Log().Errorf("Failure write data into archive: %s", err.Error())
+		}
+	}
+	return
 }
