c93f8bf01cde156a45e644211d988ffc80b6787f
rbr <rbr@arcor.de>
2020-05-05 14:39:56 +0200

    FEAT(sys): add system signal handler behavior

--- internal/sys/sys.go
+++ internal/sys/sys.go
@@ -3,6 +3,8 @@ package sys
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -33,9 +35,19 @@ func (sysLogger) GetContextName() string {
 
 // Exit stop the logging and exit
 func Exit(l int) {
-	fmt.Fprintf(os.Stderr, "... stop logging\n")
 	// execute registerd logging exit handler
 	log.Exit(l)
+}
+
+// StartSignalHandler run a system signal 'listener' on a new goroutine which will notify by os interrupts
+func StartSignalHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		ctx.Log().Info("handle system signal SIGTERM")
+		Exit(0) // use own exit func
+	}()
 }
 
 // validateFileExists just makes sure, that the path provided is a file,
